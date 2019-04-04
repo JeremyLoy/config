fee5f56e18872143401d8bb279f6a6dc7a0fa176
Jeremy Loy <jeremy.b.loy@icloud.com>
2019-04-04 15:14:37 -0400

    simplified example tests

--- example_test.go
+++ example_test.go
@@ -2,10 +2,23 @@ package config_test
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/JeremyLoy/config"
 )
+
+type MySubConfig struct {
+	IPWhitelist []string
+}
+
+type MyConfig struct {
+	DatabaseURL string
+	Port        int
+	FeatureFlag bool
+	SubConfig   MySubConfig
+}
 
 func Example() {
 
@@ -18,13 +31,9 @@ func Example() {
 	var c MyConfig
 	config.FromEnv().To(&c)
 
-	// db://
 	fmt.Println(c.DatabaseURL)
-	// 1234
 	fmt.Println(c.Port)
-	// true
 	fmt.Println(c.FeatureFlag)
-	// [0.0.0.0 1.1.1.1 2.2.2.2] 3
 	fmt.Println(c.SubConfig.IPWhitelist, len(c.SubConfig.IPWhitelist))
 
 	// Output:
@@ -32,4 +41,28 @@ func Example() {
 	// 1234
 	// true
 	// [0.0.0.0 1.1.1.1 2.2.2.2] 3
+}
+
+func Example_fromFileWithOverride() {
+	tempFile, _ := ioutil.TempFile("", "temp")
+	tempFile.Write([]byte(strings.Join([]string{"PORT=1234", "FEATUREFLAG=true"}, "\n")))
+	tempFile.Close()
+
+	os.Setenv("DATABASEURL", "db://")
+	os.Setenv("PORT", "5678")
+
+	var c MyConfig
+	config.From(tempFile.Name()).FromEnv().To(&c)
+
+	// db:// was only set in ENV
+	fmt.Println(c.DatabaseURL)
+	// 1234 was overridden by 5678
+	fmt.Println(c.Port)
+	// FeatureFlag was was only set in file
+	fmt.Println(c.FeatureFlag)
+
+	// Output:
+	// db://
+	// 5678
+	// true
 }
